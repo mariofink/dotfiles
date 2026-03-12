e089296b9de9cfc6a143240e5115f8de6215cb9d
Mario Fink <mariofink@users.noreply.github.com>
2026-03-12 12:41:01 +0100

    switch from starship to oh-my-posh

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,6 @@ fenv source ~/.profile
 # You must call it on initialization or listening to directory switching won't work
 load_nvm > /dev/stderr
 
-starship init fish | source
+oh-my-posh init fish --config ~/dotfiles/theme_oh-my-posh.json | source
 
 status --is-interactive; and source (jenv init -|psub)
